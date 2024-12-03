24d041ef1ad11da2cf040ac2d4949edccfcc05c1
Bhavitha Singamsetty <bhavitha.singamsetty@Bhavithas-MacBook-Air.local>
2024-12-03 13:49:54 -0600

    Second commit from Bhavitha Singamsetty

--- src/components/SpeechRecognition/SpeechRecognition.jsx
+++ src/components/SpeechRecognition/SpeechRecognition.jsx
@@ -1,42 +1,4 @@
 // //Author: Bhavitha Singamsetty
-//
-//
-// import React, { useEffect } from "react";
-// import SpeechRecognition, {
-//   useSpeechRecognition,
-// } from "react-speech-recognition";
-// import { IconMicrophone } from "@tabler/icons-react";
-//
-// const SpeechRecognitionComponent = ({ setSourceText }) => {
-//   const { transcript, listening } = useSpeechRecognition();
-//
-//   useEffect(() => {
-//     setSourceText(transcript);
-//   }, [transcript, setSourceText]);
-//
-//   const handleVoiceRecording = () => {
-//     if (listening) {
-//       SpeechRecognition.stopListening();
-//     } else {
-//       SpeechRecognition.startListening({ continuous: true });
-//     }
-//   };
-//
-//   return (
-//     <div>
-//       <IconMicrophone
-//         size={22}
-//         className="text-gray-400"
-//         onClick={handleVoiceRecording}
-//       />
-//     </div>
-//   );
-// };
-//
-// export default SpeechRecognitionComponent;
-
-
-
 
 import React, { useEffect } from "react";
 import SpeechRecognition, {
