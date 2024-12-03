ed667773d16502b04b720099a4d582e36a907b8c
rohithdacharla <rohithd.chowdary@gmail.com>
2024-12-03 14:57:03 -0600

    Final Commit by Rohith Dacharla

--- src/components/Buttons/IconButton.jsx
+++ src/components/Buttons/IconButton.jsx
@@ -1,3 +1,18 @@
+//Author :: Rohith Dacharla
+
+// import React from "react";
+
+// const IconButton = ({ Icon, onClick }) => (
+//   <span
+//     className="cursor-pointer flex items-center space-x-2"
+//     onClick={onClick}
+//   >
+//     <Icon size={22} />
+//   </span>
+// );
+
+// export default IconButton;
+
 import React from "react";
 
 const IconButton = ({ Icon, onClick }) => (
--- src/components/Inputs/FileUpload.jsx
+++ src/components/Inputs/FileUpload.jsx
@@ -1,3 +1,23 @@
+//Author :: Rohith Dacharla
+
+// import React from "react";
+// import { IconPaperclip } from "@tabler/icons-react";
+
+// const FileUpload = ({ handleFileUpload }) => (
+//   <label htmlFor="file-upload" className="cursor-pointer">
+//     <IconPaperclip size={21} />
+//     <input
+//       type="file"
+//       id="file-upload"
+//       onChange={handleFileUpload}
+//       className="hidden"
+//     />
+//   </label>
+// );
+
+// export default FileUpload;
+
+
 import React from "react";
 import { IconPaperclip } from "@tabler/icons-react";
 
